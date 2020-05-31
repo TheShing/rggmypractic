54fabdc83e047300cbee97479a71fb69c8dd8ff0
TheShing <artyom.rantsev@gmail.com>
2020-05-31 20:38:13 +0300

    Add files via upload

---
+++ js/index.js
@@ -0,0 +1,18 @@
+"use strict";
+
+ $(window).on('load', function () {
+	//------------------------------------------------------------------------
+	//						PRELOADER SCRIPT
+	//------------------------------------------------------------------------
+	$("#preloader").delay(400).fadeOut("slow");
+	$("#preloader .clock").fadeOut();
+});
+
+window.addEventListener('load', function() {
+
+
+
+				var audio = document.getElementById("audiohtml");
+				audio.volume = 0.1;
+				
+});
---
+++ js/voenocherk.js
@@ -0,0 +1,23 @@
+"use strict";
+
+ $(window).on('load', function () {
+	//------------------------------------------------------------------------
+	//						PRELOADER SCRIPT
+	//------------------------------------------------------------------------
+	$("#preloader").delay(400).fadeOut("slow", function() {
+		AOS.init({
+			easing: 'ease-in-out-sine'
+		});
+});
+	$("#preloader .clock").fadeOut();
+});
+
+window.addEventListener('load', function() {
+
+
+
+
+			var audio = document.getElementById("audiohtml");
+			audio.volume = 0.1;
+			
+});
